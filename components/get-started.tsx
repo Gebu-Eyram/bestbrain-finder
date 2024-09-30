1cd2570d8eef21a9e72010a41034ba960f832ed4
Gebu Eyram Henry <gebueyram18@gmail.com>
2024-09-30 22:11:06 +0000

    Resetting stuff for deplyment

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -2,17 +2,18 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const GetStarted = () => {
   const { user } = useKindeBrowserClient();
-  return (
-    <Link
-      href={`${user ? "/map" : "/api/auth/login"}`}
-      className="max-sm:w-full max-w-[400px]"
-    >
+  return user ? (
+    <Link href="/map">
       <Button className=" max-sm:w-full max-w-[400px]">Get Started</Button>
     </Link>
+  ) : (
+    <LoginLink>
+      <Button className=" max-sm:w-full max-w-[400px]">Get Started</Button>
+    </LoginLink>
   );
 };
 
