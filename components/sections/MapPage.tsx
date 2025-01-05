0da4907fe5c5e673f5d8351786a82ace5cceab81
Gebu-Eyram <123836088+Gebu-Eyram@users.noreply.github.com>
2025-01-05 14:35:31 +0000

    Revert "Remove unused 'places' prop from MapComponent in MapPage"
    This reverts commit 90776a80e2710463ff1bd014c202afc5c7a045fa.

--- components/sections/MapPage.tsx
+++ components/sections/MapPage.tsx
@@ -129,7 +129,6 @@ export function MapPage() {
           </div>
           <div className="relative  flex h-full min-h-[50vh]  flex-col lg:rounded-xl bg-muted/50  ">
             <MapComponent
-              schoolLocations={schoolLocations}
               range={range}
               setRange={(value: any) => setRange(value)}
             />
