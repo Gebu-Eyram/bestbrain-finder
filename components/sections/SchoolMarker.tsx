61a3b7642e28eddd31c77861b89cb74f1e41f90b
Gebu-Eyram <123836088+Gebu-Eyram@users.noreply.github.com>
2025-01-05 14:28:46 +0000

    Refactor SchoolMarker component to update icon handling and improve layout for active place display

--- components/sections/SchoolMarker.tsx
+++ components/sections/SchoolMarker.tsx
@@ -17,9 +17,9 @@ const SchoolMarker = ({ place, activePlace, setActivePlace }: Props) => {
       position={place.geometry.location}
       onClick={() => setActivePlace(place)}
       icon={{
-        url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=AIzaSyCjW673RoO9Lan2do3badZaOMfdQcFxn84`,
+        url: "/school.png",
         // @ts-ignore
-        scaledSize: { width: 30, height: 30 },
+        scaledSize: { width: 0, height: 0 },
 
         // Make the icon rounded
       }}
@@ -32,20 +32,42 @@ const SchoolMarker = ({ place, activePlace, setActivePlace }: Props) => {
         position={place.geometry.location}
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
       >
-        <div
-          className={`ml-[-90px]  relative mt-[-310px] ${
-            place === activePlace ? "" : "hidden"
-          }`}
-        >
-          <Button
-            size={"icon"}
-            className="absolute "
-            variant={"ghost"}
-            onClick={() => setActivePlace("")}
+        <div className="">
+          <div>
+            <div
+              className={`w-12 h-12 rounded-full shadow bg-white flex items-center justify-center`}
+            >
+              <div
+                onClick={() => setActivePlace(place)}
+                className="w-10 h-10 flex items-center justify-center cursor-pointer rounded-full bg-[#FFA500]"
+              >
+                <img
+                  src={
+                    photoRef
+                      ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=AIzaSyCjW673RoO9Lan2do3badZaOMfdQcFxn84`
+                      : "/school.png"
+                  }
+                  alt="school"
+                  className="h-10 w-10 rounded-full m-auto"
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            className={`ml-[-70px]  relative mt-[-310px] ${
+              place === activePlace ? "" : "hidden"
+            }`}
           >
-            <X className="w-4 h-4 " />
-          </Button>
-          <SchoolCard close place={place} />
+            <Button
+              size={"icon"}
+              className="absolute "
+              variant={"ghost"}
+              onClick={() => setActivePlace("")}
+            >
+              <X className="w-4 h-4 " />
+            </Button>
+            <SchoolCard close place={place} />
+          </div>
         </div>
       </OverlayView>
     </MarkerF>
